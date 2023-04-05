304084da3e0b4527ca723fdf3999a0660570418d
cn-kali-team <kali-team@qq.com>
2023-04-05 11:02:45 +0800

    fix

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,24 +1,24 @@
-#[cfg(target_os = "windows")]
-use winres;
-
 // only build for windows
 #[cfg(target_os = "windows")]
 fn main() {
     #[cfg(not(feature = "cli"))]
     tauri_build::build();
     #[cfg(feature = "cli")]
-    use std::io::Write;
-    // only build the resource for release builds
-    // as calling rc.exe might be slow
-    if std::env::var("PROFILE").unwrap() == "release" {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("icons\\icon.ico");
-        match res.compile() {
-            Err(e) => {
-                write!(std::io::stderr(), "{}", e).unwrap();
-                std::process::exit(1);
+    {
+        use winres;
+        use std::io::Write;
+        // only build the resource for release builds
+        // as calling rc.exe might be slow
+        if std::env::var("PROFILE").unwrap() == "release" {
+            let mut res = winres::WindowsResource::new();
+            res.set_icon("icons\\icon.ico");
+            match res.compile() {
+                Err(e) => {
+                    write!(std::io::stderr(), "{}", e).unwrap();
+                    std::process::exit(1);
+                }
+                Ok(_) => {}
             }
-            Ok(_) => {}
         }
     }
 }
